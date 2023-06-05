173e4fb677adc6ab75cdc5b5f2664250b89a6494
efrat-roth <efratroth15@g.jct.ac.il>
2023-06-05 22:45:30 +0300

    Update employee entity

--- server/models/employeeModel.js
+++ server/models/employeeModel.js
@@ -3,10 +3,27 @@
 const mongoose = require("mongoose");
 
 const employeeSchema = new mongoose.Schema({
+    id:int,
     first_name : String,
-    last_name: String
-    // have to continue the fields
+    last_name: String,
+    role: Role,
+    department: String,
+    email: String,
+    phone_number:String,
+    start_date:Date,
+    tags:String,
+    frontally: Boolean,
+    preferences:preferencesModel
 
 })
+const Role=[
+    "Manager", 
+    "Employee",
+    "TeamManager",
+    "Receptionist",
+    "SuperVisor",
+    "Temporary"
+];
+
 
 module.exports = mongoose.model('entries', employeeSchema);
