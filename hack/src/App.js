5eb71a660fc9e879d4adeaff7ababc69d72014bc
avishaco100 <ac0583210372@gmail.com>
2023-06-08 19:52:04 +0300

    span in about

--- hack/src/App.js
+++ hack/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 //import Text from  './Text';
 import About from './About'
-
+import HomePage from './home'
+import BookResourcePage from './BookResourcePage';
 export default function App() {
   return (
     <div className="App">
-      
-    <About/>
+    <HomePage/>
+    {/* <About/>
+    <BookResourcePage /> */}
     </div>
   );
 }
