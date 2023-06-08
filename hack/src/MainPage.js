52a44403363368cb00e35d227576779d6339d4d5
avishaco100 <ac0583210372@gmail.com>
2023-06-09 02:41:00 +0300

    react

--- hack/src/MainPage.js
+++ hack/src/MainPage.js
@@ -1,6 +1,7 @@
 import {Button,Box} from "@mui/material";
 import styled from 'styled-components';
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const MainScreen = styled.div`
@@ -41,19 +42,27 @@ function MainPage() {
     fetch("http://localhost:8000/employee/all").then(rep => rep.json()).then(jsons => setEmployees(jsons))
   }
 
+  const navigate = useNavigate();
+
   return (
     <div>
       <img style={{ width: '100%', height: '22vh' }} src="./savas.jpg" alt="Workspace Image"/>
       <h1 style={{ fontSize: '70px', color: '#00008B' }}>OptiSpace</h1>
       <div>
         <Box>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>About</Button>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>Book a Resource</Button>
+          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }} onClick={navigate(`/about}`)}>About</Button>
+          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }} onClick={navigate(`/about}`)}>Book a Resource</Button>
           <Button style={{ fontSize: '15px' ,gap: '4px', color: '#8B4513' }} onClick={handleClickEvents}>Scedule</Button>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>New Employee</Button>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>CreateBoard</Button>
+          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }} onClick={navigate(`/about}`)}>New Employee</Button>
+          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }} onClick={navigate(`/about}`)}>CreateBoard</Button>
         </Box>
         <div>{employees.map((employee, i) => <div key={i}>employ {i}-{employee.email}</div>)}</div>
+        <button style={{ position: 'absolute',
+      height: '20vh',
+      top: '10px',
+      left: '10px',
+      color: '',
+      padding: '10px', }}>connection</button>
       </div>
     </div>
   );
