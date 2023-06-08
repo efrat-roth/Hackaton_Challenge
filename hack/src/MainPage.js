f777b30d7b94aac1023bb8be0e1f5b7304e4b29f
efrat-roth <efratroth15@g.jct.ac.il>
2023-06-09 02:40:35 +0300

    react

--- hack/src/MainPage.js
+++ hack/src/MainPage.js
@@ -1,7 +1,7 @@
-import {Button,Box} from "@mui/material";
-import styled from 'styled-components';
+import { Button, Box } from "@mui/material";
+import styled from "styled-components";
 import React, { useState } from "react";
-
+import { getOptimizatiom } from "./apiClient";
 
 const MainScreen = styled.div`
   width: 100%;
@@ -18,46 +18,73 @@ const ImageContainer = styled.div`
 
 const Image = styled.img`
   width: 100%;
-  height: 22vh
+  height: 22vh;
 `;
 
 const App = () => {
   return (
     <MainScreen>
       <ImageContainer>
-        <Image src= "./savas.jpg" alt="Workspace Image"  />
-        
+        <Image src="./savas.jpg" alt="Workspace Image" />
       </ImageContainer>
     </MainScreen>
   );
 };
 
-
 function MainPage() {
- const [employees, setEmployees] = useState([])
+  const [employees, setEmployees] = useState([]);
 
-
-  function handleClickEvents(){
-    fetch("http://localhost:8000/employee/all").then(rep => rep.json()).then(jsons => setEmployees(jsons))
+  function handleClickEvents() {
+    fetch("http://localhost:8000/employee/all")
+      .then((rep) => rep.json())
+      .then((jsons) => setEmployees(jsons));
+  }
+  function schedulehandleClickEvents() {
+    getOptimizatiom();
   }
 
   return (
     <div>
-      <img style={{ width: '100%', height: '22vh' }} src="./savas.jpg" alt="Workspace Image"/>
-      <h1 style={{ fontSize: '70px', color: '#00008B' }}>OptiSpace</h1>
+      <img
+        style={{ width: "100%", height: "22vh" }}
+        src="./savas.jpg"
+        alt="Workspace Image"
+      />
+      <h1 style={{ fontSize: "70px", color: "#00008B" }}>OptiSpace</h1>
       <div>
         <Box>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>About</Button>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>Book a Resource</Button>
-          <Button style={{ fontSize: '15px' ,gap: '4px', color: '#8B4513' }} onClick={handleClickEvents}>Scedule</Button>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>New Employee</Button>
-          <Button style={{ fontSize: '15px', gap: '4px' , color: '#8B4513' }}>CreateBoard</Button>
+          <Button style={{ fontSize: "15px", gap: "4px", color: "#8B4513" }}>
+            About
+          </Button>
+          <Button style={{ fontSize: "15px", gap: "4px", color: "#8B4513" }}>
+            Book a Resource
+          </Button>
+          <Button
+            style={{ fontSize: "15px", gap: "4px", color: "#8B4513" }}
+            onClick={handleClickEvents}
+          >
+            Scedule
+          </Button>
+          <Button style={{ fontSize: "15px", gap: "4px", color: "#8B4513" }}>
+            New Employee
+          </Button>
+          <Button
+            style={{ fontSize: "15px", gap: "4px", color: "#8B4513" }}
+            onClick={schedulehandleClickEvents}
+          >
+            CreateBoard
+          </Button>
         </Box>
-        <div>{employees.map((employee, i) => <div key={i}>employ {i}-{employee.email}</div>)}</div>
+        <div>
+          {employees.map((employee, i) => (
+            <div key={i}>
+              employ {i}-{employee.email}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
-
 
 export default MainPage;
